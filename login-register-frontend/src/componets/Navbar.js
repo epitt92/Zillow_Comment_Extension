11af2c998cbdabcc199327229c6931cc77afc073
epitt92 <info.epitt92@gmail.com>
2023-01-30 16:18:31 +0900

    Add the hashrouter

--- login-register-frontend/src/componets/Navbar.js
+++ login-register-frontend/src/componets/Navbar.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
-import { logoutAction } from '../redux/slices/authSlice';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { logoutAction } from "../redux/slices/authSlice";
 import "./Loginregister.css";
 import {
   MDBContainer,
@@ -18,56 +18,60 @@ import {
   MDBDropdownMenu,
   MDBDropdownItem,
   MDBCollapse,
-} from 'mdb-react-ui-kit';
+} from "mdb-react-ui-kit";
 
-const Navbar = () =>{
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+const Navbar = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const logout = () => {
-        dispatch(logoutAction({navigate}));
-    }
+  const logout = () => {
+    dispatch(logoutAction({ navigate }));
+  };
 
-    const { authFlag, profile } = useSelector((state) => ({
-        authFlag : state.auth.authFlag, 
-        profile : state.auth.profile
-    }));
-    return(
-      <>
-        <MDBNavbar expand='lg' light bgColor='light'>
-          <MDBContainer fluid>
-            <MDBNavbarBrand className='ps-2' href='#'>Zillow Admin</MDBNavbarBrand>
-            <MDBCollapse navbar >
-              <MDBNavbarNav>
-                <MDBNavbarItem>
-                  <MDBNavbarLink active aria-current='page' href='/'>
-                    Home
-                  </MDBNavbarLink>
-                </MDBNavbarItem>
-                <MDBNavbarItem>
-                  <MDBNavbarLink href='/'>Comments</MDBNavbarLink>
-                </MDBNavbarItem>
-                <MDBNavbarItem>
-                  <MDBNavbarLink href='/users'>Users</MDBNavbarLink>
-                </MDBNavbarItem>
-              </MDBNavbarNav>
-            </MDBCollapse>
+  const { authFlag, profile } = useSelector((state) => ({
+    authFlag: state.auth.authFlag,
+    profile: state.auth.profile,
+  }));
+  return (
+    <>
+      <MDBNavbar expand="lg" light bgColor="light">
+        <MDBContainer fluid>
+          <MDBNavbarBrand className="ps-2" href="#">
+            Zillow Admin
+          </MDBNavbarBrand>
+          <MDBCollapse navbar>
+            <MDBNavbarNav>
+              <MDBNavbarItem>
+                <MDBNavbarLink active aria-current="page" href="/">
+                  Home
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href="/">Comments</MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href="/#/users">Users</MDBNavbarLink>
+              </MDBNavbarItem>
+            </MDBNavbarNav>
+          </MDBCollapse>
 
-            <MDBCollapse navbar>
-              <MDBNavbarNav right fullWidth={false} className='pe-5 mb-2 mb-lg-0'>
-                <MDBNavbarItem>
-                  <MDBNavbarLink active aria-current='page' href='#'>
-                    Welcome {profile.name}
-                  </MDBNavbarLink>
-                </MDBNavbarItem>
-                <MDBNavbarItem>
-                  <MDBBtn className="btn-logout" color='danger' onClick={logout}>Logout</MDBBtn>
-                </MDBNavbarItem>
-              </MDBNavbarNav>
-            </MDBCollapse>
-          </MDBContainer>
-        </MDBNavbar>
-      </>
-    );
-}
+          <MDBCollapse navbar>
+            <MDBNavbarNav right fullWidth={false} className="pe-5 mb-2 mb-lg-0">
+              <MDBNavbarItem>
+                <MDBNavbarLink active aria-current="page" href="#">
+                  Welcome {profile.name}
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBBtn className="btn-logout" color="danger" onClick={logout}>
+                  Logout
+                </MDBBtn>
+              </MDBNavbarItem>
+            </MDBNavbarNav>
+          </MDBCollapse>
+        </MDBContainer>
+      </MDBNavbar>
+    </>
+  );
+};
 export default Navbar;
