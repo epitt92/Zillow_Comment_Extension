2c28c833ea2eb7f9d01507cc547c9f6bbddefd04
epitt92 <info.epitt92@gmail.com>
2023-01-30 15:59:16 +0900

    Add the hashrouter

--- login-register-frontend/src/App.js
+++ login-register-frontend/src/App.js
@@ -1,46 +1,42 @@
-import './App.css';
-import { useEffect, useState } from 'react';
-import Homepage from './componets/Homepage';
-import Users from './componets/Users';
-import Login from './componets/Login';
-import Register from './componets/Register';
-import { BrowserRouter,Route, Routes } from "react-router-dom";
-import { useDispatch, useSelector } from 'react-redux';
-import { setAuthFlag, setProfile } from './redux/slices/authSlice';
+import "./App.css";
+import { useEffect, useState } from "react";
+import Homepage from "./componets/Homepage";
+import Users from "./componets/Users";
+import Login from "./componets/Login";
+import Register from "./componets/Register";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { setAuthFlag, setProfile } from "./redux/slices/authSlice";
 
 function App() {
-
   const dispatch = useDispatch();
 
   const { authFlag } = useSelector((state) => ({
-    authFlag : state.auth.authFlag
+    authFlag: state.auth.authFlag,
   }));
   useEffect(() => {
-    if(localStorage.profile && localStorage.authFlag){
+    if (localStorage.profile && localStorage.authFlag) {
       let profile = JSON.parse(localStorage.profile);
-      let authFlag = (localStorage.authFlag === 'true');
-      if (authFlag){
+      let authFlag = localStorage.authFlag === "true";
+      if (authFlag) {
         dispatch(setAuthFlag(authFlag));
         dispatch(setProfile(profile));
       }
     }
-  }, [])
+  }, []);
   return (
     <>
-      <BrowserRouter>
+      <Router>
         <Routes>
-          <Route path='/' 
-          element={
-          authFlag ? <Homepage /> : <Login />
-          } ></Route>
-          <Route path='/users' 
-          element={
-          authFlag ? <Users /> : <Login />
-          } ></Route>
-          <Route path='/login' element= {<Login />} ></Route>
-          <Route path='/register' element={<Register />} ></Route>
+          <Route path="/" element={authFlag ? <Homepage /> : <Login />}></Route>
+          <Route
+            path="/users"
+            element={authFlag ? <Users /> : <Login />}
+          ></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
         </Routes>
-      </BrowserRouter>
+      </Router>
     </>
   );
 }
