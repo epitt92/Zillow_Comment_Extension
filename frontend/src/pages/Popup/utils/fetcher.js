f741ac7ae96c21986dcce01bbf0dfc8623df8102
epitt92 <info.epitt92@gmail.com>
2023-01-30 14:33:54 +0900

    Update the development to production

--- frontend/src/pages/Popup/utils/fetcher.js
+++ frontend/src/pages/Popup/utils/fetcher.js
@@ -1,27 +1,27 @@
-import axios from "axios";
-import Config from "../../Config";
+import axios from 'axios';
+import Config from '../../Config';
 
 const urlBase =
-  false
-    ? Config.LOCAL_BACKEND_URL + "/api"
-    : Config.BACKEND_URL + "/api";
+  Config.NODE_ENV === 'development'
+    ? Config.LOCAL_BACKEND_URL + '/api'
+    : Config.BACKEND_URL + '/api';
 
 export const apiCaller = axios.create({
   baseURL: urlBase,
   headers: {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*",
-    credentials: "omit"
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    credentials: 'omit',
   },
   withCredentials: false,
 });
 
 export const getErrorMessage = (err) => {
-  let message = "";
+  let message = '';
   try {
     message = err.response.data.message;
   } catch {
-    message = "Something went wrong!";
+    message = 'Something went wrong!';
   }
   return message;
 };
